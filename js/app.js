7912fa071cec2d4d9b484cccc141471e607bd94f
Agasthian <agasthian_r@hotmail.com>
2021-06-27 09:27:21 +0530

    Fixed navigation links, gsap animations & footer layouts

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 //******* FullPage.js Animation Scroll- custom script *****
  new fullpage('#fullpage', {
    //options
-  //  anchors:['section1', 'section2', 'section3','section4', 'section5', 'section6'],
+   anchors:['section1', 'section2', 'section3','section4', 'section5', 'section6'],
    scrollingSpeed: 900,
    autoScrolling:true,
    navigation:true,
@@ -13,18 +13,9 @@
      tl.fromTo( title, 0.5, {y:"50", opacity:0}, {y:"0", opacity:1});
 
      if(desitination.index === 1){
-      const hero = document.querySelector('.hero-wrapper__content');
-      const slider = document.querySelector('.slider_bg');
-      const logo = document.querySelector('.logo_img');
-      const hamburger = document.querySelector('.hamburger');
       const headline = document.querySelector('.hero-wrapper__content__text');
      
-      tl.fromTo(hero,1, {height: "0%"}, {height:"95%", ease: Power2.easeInOut})
-        .fromTo(hero,1.2, {width: "100%"}, {width :"80%", ease: Power2.easeInOut})
-        .fromTo(slider,1.2,{ x : "-100%"}, {x:'0%', ease: Power2.easeInOut}, '-=1.2')
-        .fromTo( logo, 0.5, {opacity: 0, y:-30}, {opacity:1, y:0}, '-=0.5')
-        .fromTo( hamburger,  0.5, {opacity: 0, x:30}, {opacity:1, x:0}, '-=0.5')
-        .fromTo( headline,  0.5, {opacity: 0, y:40}, {opacity:1, y:0}, '-=0.5')
+      tl.fromTo( headline,  0.5, {opacity: 0, y:40}, {opacity:1, y:0}, '-=0.5')
      }
    }
  });
