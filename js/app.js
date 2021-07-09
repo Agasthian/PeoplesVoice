af8149116bef405cf3f2bea2aed02a3e5760330c
Agasthian <agasthian_r@hotmail.com>
2021-07-09 19:59:21 +0530

    Fixed slick slider responsivess bug

--- js/app.js
+++ js/app.js
@@ -56,7 +56,34 @@ $('.cardWrapper').slick({
   slidesToScroll: 1,
   dots: true,
   focusOnSelect: true,
-   
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
   
 });
 
@@ -68,5 +95,35 @@ $('.project-slider').slick({
   autoplay:true,
   autoplaySpeed:2500,
   speed: 1000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        centerMode: true,
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        centerMode: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
   
 });
