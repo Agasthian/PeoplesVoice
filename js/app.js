8780bb9ca608895c762ca1c50a7d616a8bfb8a4d
Agasthian <agasthian_r@hotmail.com>
2021-08-12 20:07:08 +0530

    Two Section code redesign in Grid

--- js/app.js
+++ js/app.js
@@ -1,55 +1,56 @@
 //******* FullPage.js Animation Scroll- custom script *****
 new fullpage('#fullpage', {
   //options
-  // anchors: [
-  //   'section1',
-  //   'section2',
-  //   'section3',
-  //   'section4',
-  //   'section5',
-  //   'section6',
-  // ],
+  anchors: [
+    'section1',
+    'section2',
+    'section3',
+    'section4',
+    'section5',
+    'section6',
+  ],
 
   //Scrolling
   scrollingSpeed: 900,
   autoScrolling: true,
   navigation: true,
   easing: 'easeInOutCubic',
+
   //Custom selector
   //lazyLoading: true,
   // Design
   responsiveWidth: 425,
 
-  // onLeave: (origin, desitination, direction) => {
-  //   const section = desitination.item;
-  //   const title = section.querySelector('h1');
-  //   const titleh3 = section.querySelector('h3');
-  //   const cardwrapper = document.querySelector('.cardWrapper');
-  //   const sectionpara = document.querySelector('.paraS3');
+  onLeave: (origin, desitination, direction) => {
+    const section = desitination.item;
+    const title = section.querySelector('h1');
+    const titleh3 = section.querySelector('h3');
+    const cardwrapper = document.querySelector('.cardWrapper');
+    const sectionpara = document.querySelector('.paraS3');
 
-  //   const tl = new TimelineMax({ delay: 1 });
-  //   tl.fromTo(title, 0.5, { y: '50', opacity: 0 }, { y: '0', opacity: 1 })
-  //     .fromTo(titleh3, 0.5, { y: '50', opacity: 0 }, { y: '0', opacity: 1 })
-  //     .fromTo(
-  //       sectionpara,
-  //       0.5,
-  //       { opacity: 0, y: 40 },
-  //       { opacity: 1, y: 0 },
-  //       '-=0.5'
-  //     );
+    const tl = new TimelineMax({ delay: 1 });
+    tl.fromTo(title, 0.5, { y: '50', opacity: 0 }, { y: '0', opacity: 1 })
+      .fromTo(titleh3, 0.5, { y: '50', opacity: 0 }, { y: '0', opacity: 1 })
+      .fromTo(
+        sectionpara,
+        0.5,
+        { opacity: 0, y: 40 },
+        { opacity: 1, y: 0 },
+        '-=0.5'
+      );
 
-  //   if (desitination.index === 1) {
-  //     const headline = document.querySelector('.hero-wrapper__content__text');
+    if (desitination.index === 1) {
+      const headline = document.querySelector('.hero-wrapper__content__text');
 
-  //     tl.fromTo(
-  //       headline,
-  //       0.5,
-  //       { opacity: 0, y: 40 },
-  //       { opacity: 1, y: 0 },
-  //       '-=0.5'
-  //     ).fromTo(cardwrapper, 0.5, { opacity: 0, y: 60 }, { opacity: 1, y: 0 });
-  //   }
-  // },
+      tl.fromTo(
+        headline,
+        0.5,
+        { opacity: 0, y: 40 },
+        { opacity: 1, y: 0 },
+        '-=0.5'
+      ).fromTo(cardwrapper, 0.5, { opacity: 0, y: 60 }, { opacity: 1, y: 0 });
+    }
+  },
 });
 
 //**************** GSAP animation**************
